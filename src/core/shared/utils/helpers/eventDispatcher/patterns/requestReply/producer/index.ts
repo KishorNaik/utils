c391f20aaf2b855b3a691d515deaba8158222daa
Kishor Naik <kishor.naik011.net@gmail.com>
2025-09-23 11:37:59 +0530

    @KishorNaik - Update Event Dispatcher

--- src/core/shared/utils/helpers/eventDispatcher/patterns/requestReply/producer/index.ts
+++ src/core/shared/utils/helpers/eventDispatcher/patterns/requestReply/producer/index.ts
@@ -6,24 +6,13 @@ export class RequestReplyProducerEventDispatcher {
 
 	public async sendAsync<TRequest, TReply>(
 		eventType: string,
-		data: TRequest,
-		meta: Partial<RequestReplyEventDispatcher<TRequest>>
+		data: RequestReplyEventDispatcher<TRequest>
 	): Promise<ReplyMessageEventDispatcher<TReply>> {
 		if (!eventType) throw new Error('Event type is required');
 		if (!data) throw new Error('Data is required');
-		if (!meta) throw new Error('Meta is required');
 
-		const correlationId = meta?.correlationId ?? crypto.randomUUID();
-		const traceId = meta?.traceId ?? crypto.randomUUID();
-		const timestamp = meta?.timestamp ?? new Date().toISOString();
-		const replyType = `reply:${correlationId}`;
 
-		const event: RequestReplyEventDispatcher<TRequest> = {
-			data,
-			correlationId,
-			traceId,
-			timestamp,
-		};
+		const replyType = `reply:${data.correlationId}`;
 
 		return new Promise<ReplyMessageEventDispatcher<TReply>>((resolve, reject) => {
 			const replyHandler = async (
@@ -41,7 +30,7 @@ export class RequestReplyProducerEventDispatcher {
 			};
 
 			this.dispatcher.onReplyOnce(replyType, replyHandler);
-			this.dispatcher.publishRequest(eventType, event);
+			this.dispatcher.publishRequest(eventType, data);
 		});
 	}
 }
--- src/core/shared/utils/helpers/eventDispatcher/patterns/sendReceiver/producer/index.ts
+++ src/core/shared/utils/helpers/eventDispatcher/patterns/sendReceiver/producer/index.ts
@@ -6,23 +6,15 @@ export class SendReceiverProducerEventDispatcher {
 
 	public async sendAsync<T>(
 		eventType: string,
-		data: T,
-		meta: Partial<SendReceiverMessageEventDispatcher<T>>,
+		data: SendReceiverMessageEventDispatcher<T>,
 		replayable: ReplayableEventDispatcher = ReplayableEventDispatcher.NO
 	): Promise<void> {
 		if (!eventType) throw new Error('Event type is required');
 		if (!data) throw new Error('Data is required');
-		if (!meta) throw new Error('Meta is required');
 
-		const event: SendReceiverMessageEventDispatcher<T> = {
-			data,
-			traceId: meta?.traceId ?? crypto.randomUUID(),
-			correlationId: meta?.correlationId ?? crypto.randomUUID(),
-			timestamp: meta?.timestamp ?? new Date().toISOString(),
-		};
 		await this.dispatcher.publish(
 			eventType,
-			event,
+			data,
 			replayable === ReplayableEventDispatcher.YES
 		);
 	}
